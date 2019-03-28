b71ce6a19bdc8981a227eb16625daba97eb7433e
kino <378907426@qq.com>
2019-03-28 23:38:44 +0800

    使用egg-session

--- config/config.default.js
+++ config/config.default.js
@@ -17,8 +17,8 @@ exports.view = {
 
 exports.session = {
   key: 'EGG_SESS',
-  // maxAge: 0.5 * 3600 * 1000, // 30分钟
-  maxAge: 10000, // 10秒
+  maxAge: 0.5 * 3600 * 1000, // 30分钟
+  // maxAge: 10000, // 10秒
   httpOnly: true,
   encrypt: true,
   renew: true
